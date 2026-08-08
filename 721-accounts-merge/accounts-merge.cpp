7fe8a60652b6612e0762c668c1f6950922ba0093
Varun Gupta <varun.vip2811@gmail.com>
2026-08-08 23:27:12 +0530

    Time: 32 ms (55.93%) | Memory: 40.2 MB (74.95%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    vector<int>parent;
-    vector<int> rank;
+    vector<int> parent;
+    vector<int> size;
     int find(int node){
         if(node==parent[node]){
             return node;
@@ -11,49 +11,49 @@ public:
     void Union(int u,int v){
         int pu=find(u);
         int pv=find(v);
-        
+
         if(pu==pv) return;
-        
-        if(rank[pu]>rank[pv]){
+
+        if(size[pu]>size[pv]){
             parent[pv]=pu;
-        }else if(rank[pu]<rank[pv]){
-            parent[pu]=pv;
+            size[pu]+=size[pv];
         }else{
             parent[pu]=pv;
-            rank[pv]++;
+            size[pv]+=size[pu];
         }
     }
-    vector<vector<string>> accountsMerge(vector<vector<string>>& details) {
-        parent.resize(details.size());
-        rank.assign(details.size(),0);
-        
-        for(int i=0;i<details.size();i++) parent[i]=i;
-        
-        unordered_map<string,int> mapmailNode;
-        for(int i=0;i<details.size();i++){
-            for(int j=1;j<details[i].size();j++){
-                string mail=details[i][j];
-                if(mapmailNode.find(mail)==mapmailNode.end()){
-                    mapmailNode[mail]=i;
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        int n=accounts.size();
+        parent.resize(n);
+        size.assign(n,1);
+
+        for(int i=0;i<n;i++) parent[i]=i;
+
+        unordered_map<string,int> mp;//name,mailNo
+
+        for(int i=0;i<n;i++){
+            for(int j=1;j<accounts[i].size();j++){
+                string mail=accounts[i][j];
+                if(mp.find(mail)==mp.end()){
+                    mp[mail]=i;
                 }else{
-                    Union(i,mapmailNode[mail]);
+                    Union(i,mp[mail]);
                 }
             }
         }
-        vector<string> mergedMail[details.size()];
-        for(auto it:mapmailNode){
+        vector<string> list[n];
+        for(auto it:mp){
             string mail=it.first;
             int node=find(it.second);
-            mergedMail[node].push_back(mail);
+            list[node].push_back(mail);
         }
         vector<vector<string>> ans;
-        
-        for(int i=0;i<details.size();i++){
-            if(mergedMail[i].size()==0) continue;
-            sort(mergedMail[i].begin(),mergedMail[i].end());
+        for(int i=0;i<n;i++){
+            if(list[i].size()==0) continue;
+            sort(list[i].begin(),list[i].end());
             vector<string> temp;
-            temp.push_back(details[i][0]);
-            for(auto it:mergedMail[i]){
+            temp.push_back(accounts[i][0]);
+            for(auto it:list[i]){
                 temp.push_back(it);
             }
             ans.push_back(temp);
