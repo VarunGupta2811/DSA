d55b884a16f3d1e7ea5a8af3cbba8b94a590a035
Varun Gupta <varun.vip2811@gmail.com>
2026-08-09 09:59:51 +0530

    Time: 27 ms (76.60%) | Memory: 40.2 MB (74.95%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -4,7 +4,7 @@ public:
     vector<int> size;
     int find(int node){
         if(node==parent[node]){
-            return node;
+            return node; 
         }
         return parent[node]=find(parent[node]);
     }
@@ -15,8 +15,8 @@ public:
         if(pu==pv) return;
 
         if(size[pu]>size[pv]){
-            parent[pv]=pu;
             size[pu]+=size[pv];
+            parent[pv]=pu;
         }else{
             parent[pu]=pv;
             size[pv]+=size[pu];
@@ -26,34 +26,31 @@ public:
         int n=accounts.size();
         parent.resize(n);
         size.assign(n,1);
-
         for(int i=0;i<n;i++) parent[i]=i;
-
-        unordered_map<string,int> mp;//name,mailNo
-
+        unordered_map<string,int> mp;
         for(int i=0;i<n;i++){
             for(int j=1;j<accounts[i].size();j++){
                 string mail=accounts[i][j];
-                if(mp.find(mail)==mp.end()){//if the current email doesn't exist already
+                if(mp.find(mail)==mp.end()){
                     mp[mail]=i;
                 }else{
-                    Union(i,mp[mail]);//if exist union of two accounts
+                    Union(i,mp[mail]);
                 }
             }
         }
-        vector<string> list[n];//will store which account has which email
+        vector<string> merged[n];
         for(auto it:mp){
             string mail=it.first;
             int node=find(it.second);
-            list[node].push_back(mail);
+            merged[node].push_back(mail);
         }
         vector<vector<string>> ans;
         for(int i=0;i<n;i++){
-            if(list[i].size()==0) continue;//if let's say two accounts get merged then one will have no emails,so just skip that account
-            sort(list[i].begin(),list[i].end());
+            if(merged[i].size()==0) continue;
+            sort(merged[i].begin(),merged[i].end());
             vector<string> temp;
             temp.push_back(accounts[i][0]);
-            for(auto it:list[i]){
+            for(auto it:merged[i]){
                 temp.push_back(it);
             }
             ans.push_back(temp);
