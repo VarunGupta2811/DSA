38f8d6cdf56b3298d4569ee534ae137f8fa18437
Varun Gupta <varun.vip2811@gmail.com>
2026-08-22 23:29:44 +0530

    Time: 35 ms (21.54%) | Memory: 23.2 MB (98.99%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    typedef pair<int,pair<int,int>> p;
     bool isValid(int i,int j,int n){
         return i>=0 && i<n && j>=0 && j<n;
     }
@@ -7,27 +8,28 @@ public:
         int n=grid.size();
         if(grid[0][0]!=0 || grid[n-1][n-1]!=0) return -1;
 
-        queue<pair<int, pair<int, int>>> q;
-        q.push({1, {0, 0}});
-        grid[0][0]=2;
-
+        vector<int> result(n*n,INT_MAX);
+        priority_queue<p,vector<p>,greater<p>>pq;
+        pq.push({1,{0,0}});
         int dr[]={-1,-1,0,1,1,1,0,-1};
         int dc[]={0,-1,-1,-1,0,1,1,1};
-
-        while(!q.empty())
+        while(!pq.empty())
         {
-            int dist=q.front().first;
-            int r=q.front().second.first;
-            int c=q.front().second.second;
+            int dist=pq.top().first;
+            int r=pq.top().second.first;
+            int c=pq.top().second.second;
+            pq.pop();
+            if(dist>result[r*n+c]) continue;
             if(r==n-1 && c==n-1) return dist;
-            q.pop();
-
             for(int ind=0;ind<8;ind++){
                 int newr=r+dr[ind];
                 int newc=c+dc[ind];
-                if(isValid(newr,newc,n) && grid[newr][newc]==0){
-                    grid[newr][newc]=2;
-                    q.push({dist + 1, {newr, newc}});
+                if(isValid(newr,newc,n)&&grid[newr][newc]==0){
+                   int adjNode=newr*n+newc;
+                   if(dist+1<result[adjNode]){
+                    result[adjNode]=dist+1;
+                    pq.push({dist+1,{newr,newc}});
+                   }
                 }
             }
         }
