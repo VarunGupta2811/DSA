a511f9c62d57ab01ba122620b7da5eafe5b30e8b
Varun Gupta <varun.vip2811@gmail.com>
2026-08-23 21:48:44 +0530

    Time: 56 ms (8.16%) | Memory: 36.4 MB (55.45%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
+    typedef pair<int,pair<int,int>> p;
     bool isValid(int i,int j,int m,int n){
         return i>=0 && i<m && j>=0 && j<n;
     }
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m=mat.size();
         int n=mat[0].size();
-        vector<vector<int>>ans(m,vector<int>(n,-1));
+        vector<vector<int>>ans(m,vector<int>(n,INT_MAX));
         int dr[]={-1,0,1,0};
         int dc[]={0,-1,0,1};
-        queue<pair<int,pair<int,int>>>q;
+        priority_queue<p,vector<p>,greater<p>>pq;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
-                    q.push({0,{i,j}});
+                    pq.push({0,{i,j}});
                     ans[i][j]=0;
                 }
             }
         }
-        while(!q.empty())
+        while(!pq.empty())
         {
-            int dist=q.front().first;
-            int r=q.front().second.first;
-            int c=q.front().second.second;
-            q.pop();
+            int dist=pq.top().first;
+            int r=pq.top().second.first;
+            int c=pq.top().second.second;
+            pq.pop();
             for(int i=0;i<4;i++){
                 int newr=r+dr[i];
                 int newc=c+dc[i];
-                if(isValid(newr,newc,m,n) && ans[newr][newc]==-1){
-                    ans[newr][newc]=dist+1;
-                    q.push({dist+1,{newr,newc}});
+                if(isValid(newr,newc,m,n)){
+                    if(ans[r][c]+1<ans[newr][newc]){
+                        ans[newr][newc]=ans[r][c]+1;
+                        pq.push({ans[newr][newc],{newr,newc}});
+                    }
                 }
             }
         }
