0a273d4359dbc629f505a6374e7736e1a633adf3
Varun Gupta <varun.vip2811@gmail.com>
2026-08-24 17:22:13 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (69.58%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -6,39 +6,40 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>>res(m,vector<int>(n,-1));
         int count=0;
+        int fresh=0;
         queue<pair<int,int>>que;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
                     que.push({i,j});
-                    res[i][j]=0;
+                }else if(grid[i][j]==1){
+                    fresh++;
                 }
             }
         }
+        if(fresh==0) return 0;
         int dr[]={-1,0,1,0};
         int dc[]={0,-1,0,1};
         while(!que.empty())
         {
-            int r=que.front().first;
-            int c=que.front().second;
-            que.pop();
-            for(int ind=0;ind<4;ind++){
-                int newr=r+dr[ind];
-                int newc=c+dc[ind];
-                if(isValid(newr,newc,m,n) && res[newr][newc]==-1  && grid[newr][newc]==1){
-                    res[newr][newc]=res[r][c]+1;
-                    count=max(count,res[newr][newc]);
-                    que.push({newr,newc});
+            int size=que.size();
+            while(size--){
+                int r=que.front().first;
+                int c=que.front().second;
+                que.pop();
+                for(int ind=0;ind<4;ind++){
+                    int newr=r+dr[ind];
+                    int newc=c+dc[ind];
+                    if(isValid(newr,newc,m,n) && grid[newr][newc]==1){
+                        grid[newr][newc]=2;
+                        que.push({newr,newc});
+                        fresh--;
+                    }
                 }
             }
+            count++;
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(res[i][j]==-1 && grid[i][j]==1) return -1;
-            }
-        }
-        return count;
+        return fresh==0?count-1:-1;
     }
 };
