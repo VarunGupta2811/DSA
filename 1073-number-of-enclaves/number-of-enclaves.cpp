5dda01ffc99ddf0c226702d59be72f4adc233f2b
Varun Gupta <varun.vip2811@gmail.com>
2026-08-26 15:56:38 +0530

    Time: 9 ms (48.34%) | Memory: 37.2 MB (24.27%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    bool isValid(int i,int j,int n,int m){
-        return i>=0 && i<n && j>=0 && j<m;
+    int n,m;
+    void dfs(int i,int j,vector<vector<int>>& grid,vector<vector<bool>>&visited){
+        if(i<0 || i>=n || j<0 || j>=m) return;
+        if(visited[i][j] || grid[i][j]==0) return;
+        visited[i][j]=true;
+        dfs(i+1,j,grid,visited);
+        dfs(i-1,j,grid,visited);
+        dfs(i,j+1,grid,visited);
+        dfs(i,j-1,grid,visited);
     }
     int numEnclaves(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
+        n=grid.size();
+        m=grid[0].size();
         vector<vector<bool>>visited(n,vector<bool>(m,false));
-        queue<pair<int,int>>q;
-        int dr[]={-1,0,1,0};
-        int dc[]={0,-1,0,1};
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if((i == 0 || i == n - 1 || j == 0 || j == m - 1) && grid[i][j] == 1) {
-                    q.push({i, j});
-                    visited[i][j] = true;
-                }
+            if(grid[i][0]==1){
+                dfs(i,0,grid,visited);
+            }
+            if(grid[i][m-1]==1){
+                dfs(i,m-1,grid,visited);
             }
         }
-        while(!q.empty())
-        {
-            int r=q.front().first;
-            int c=q.front().second;
-            q.pop();
-            for(int ind=0;ind<4;ind++){
-                int newr=r+dr[ind];
-                int newc=c+dc[ind];
-                if(isValid(newr,newc,n,m) && grid[newr][newc]==1 && !visited[newr][newc]){
-                    visited[newr][newc]=true;
-                    q.push({newr,newc});
-                }
+        for(int j=0;j<m;j++){
+            if(grid[0][j]==1){
+                dfs(0,j,grid,visited);
+            }
+            if(grid[n-1][j]==1){
+                dfs(n-1,j,grid,visited);
             }
         }
         int enclaves=0;
