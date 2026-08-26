7b94a1784739b1b37a8cf410d4a7f22c12687414
Varun Gupta <varun.vip2811@gmail.com>
2026-08-26 21:53:07 +0530

    Time: 0 ms (100.00%) | Memory: 24.1 MB (66.93%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        int n=nums.size();
-        int maxVal=nums[0];
-        unordered_map<int,bool>mp;
-        for(int i=0;i<n;i++){
-            maxVal=max(maxVal,nums[i]);
-            mp[nums[i]]=true;
+        for(int i = 0, j = k; i < nums.size(); i++)
+        {
+            if(nums[i] == k)
+            {
+                k += j;
+                i = -1;
+            }
         }
-        int i=k;
-        for(;i<=maxVal;i+=k){
-            if(mp[i]==false) return i;
-        }
-        return i;
+        return k;
     }
 };
