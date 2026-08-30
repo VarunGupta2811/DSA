d299afc85de1d900545b58d29994ada5bd6b10aa
Varun Gupta <varun.vip2811@gmail.com>
2026-08-30 22:16:51 +0530

    Time: 275 ms (79.07%) | Memory: 128.8 MB (72.62%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
+    int n;
     typedef pair<int,pair<int,int>>p;
-    bool isValid(int i,int j,int n){
+    bool isValid(int i,int j){
         return i>=0 && i<n && j>=0 && j<n;
     }
     int maximumSafenessFactor(vector<vector<int>>& grid) {
-        int n=grid.size();
-        if(grid[0][0]==1||grid[n-1][n-1]==1) return 0;
-        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
+        n=grid.size();
+        if(grid[0][0]==1 || grid[n-1][n-1]==1) return 0;
         queue<pair<int,int>>q;
+        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
@@ -21,38 +22,37 @@ public:
         int dc[]={0,-1,0,1};
         while(!q.empty())
         {
-            int r=q.front().first;
-            int c=q.front().second;
+            auto [r,c]=q.front();
             q.pop();
             for(int ind=0;ind<4;ind++){
                 int newr=r+dr[ind];
                 int newc=c+dc[ind];
-                if(isValid(newr,newc,n) && dist[newr][newc]==INT_MAX){
+                if(isValid(newr,newc) && dist[newr][newc]==INT_MAX){
                     dist[newr][newc]=dist[r][c]+1;
                     q.push({newr,newc});
                 }
             }
         }
         priority_queue<p>pq;
-        vector<vector<int>>best(n,vector<int>(n,-1));
-        best[0][0]=dist[0][0];
+        vector<vector<int>>safe(n,vector<int>(n,-1));
+        safe[0][0]=dist[0][0];
         pq.push({dist[0][0],{0,0}});
         while(!pq.empty())
         {
-            auto [safe,pos]=pq.top();
+            auto [fact,pos]=pq.top();
             int r=pos.first;
             int c=pos.second;
             pq.pop();
-            if(r==n-1 && c==n-1) return safe;
-            if(safe<best[r][c]) continue;
+            if(r==n-1 && c==n-1) return fact;
+            if(fact<safe[r][c]) continue;
             for(int ind=0;ind<4;ind++){
                 int newr=r+dr[ind];
                 int newc=c+dc[ind];
-                if(isValid(newr,newc,n)){
-                    int newSafe=min(safe,dist[newr][newc]);
-                    if(newSafe>best[newr][newc]){
-                        best[newr][newc]=newSafe;
-                        pq.push({newSafe,{newr,newc}});
+                if(isValid(newr,newc)){
+                    int newFact=min(fact,dist[newr][newc]);
+                    if(newFact>safe[newr][newc]){
+                        safe[newr][newc]=newFact;
+                        pq.push({newFact,{newr,newc}});
                     }
                 }
             }
