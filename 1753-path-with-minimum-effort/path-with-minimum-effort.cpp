34b30fe69b155bdddce9e801e4daa27223884054
Varun Gupta <varun.vip2811@gmail.com>
2026-08-31 21:30:10 +0530

    Time: 44 ms (82.41%) | Memory: 24.5 MB (77.27%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -5,28 +5,27 @@ public:
     bool isValid(int i,int j){
         return i>=0 && i<n && j>=0 && j<m;
     }
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        n=heights.size();
-        m=heights[0].size();
+    int minimumEffortPath(vector<vector<int>>& grid) {
+        n=grid.size();
+        m=grid[0].size();
         vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
         priority_queue<p,vector<p>,greater<p>>pq;
         pq.push({0,{0,0}});
-        dist[0][0]=0;
         int dr[]={-1,0,1,0};
         int dc[]={0,-1,0,1};
         while(!pq.empty())
         {
-            int diff=pq.top().first;
-            int r=pq.top().second.first;
-            int c=pq.top().second.second;
+            auto [effort,pos]=pq.top();
+            int r=pos.first;
+            int c=pos.second;
             pq.pop();
-            if(diff>dist[r][c]) continue;
-            if(r==n-1 && c==m-1) return diff;
+            if(r==n-1 && c==m-1) return effort;
+            if(effort>dist[r][c]) continue;
             for(int ind=0;ind<4;ind++){
                 int newr=r+dr[ind];
                 int newc=c+dc[ind];
                 if(isValid(newr,newc)){
-                    int newEffort=max(abs(heights[r][c]-heights[newr][newc]),diff);
+                    int newEffort=max(abs(grid[r][c]-grid[newr][newc]),effort);
                     if(newEffort<dist[newr][newc]){
                         dist[newr][newc]=newEffort;
                         pq.push({newEffort,{newr,newc}});
