404dfa5e7d973ad9abbf70e74650cc33b7a00c5f
Varun Gupta <varun.vip2811@gmail.com>
2026-09-05 20:23:50 +0530

    Time: 4 ms (59.83%) | Memory: 13.8 MB (76.13%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& graph, int currentNode, int target, vector<int>& currentPath, vector<vector<int>>& result){
-        if(currentNode==target){
-            result.push_back(currentPath);
+    void dfs(int curr,int target,vector<vector<int>>& graph,vector<int> &currPath,vector<vector<int>> &result){
+        if(curr==target){
+            result.push_back(currPath);
             return;
         }
-        for(int neighbour:graph[currentNode]){
-            currentPath.push_back(neighbour);
-            dfs(graph,neighbour,target,currentPath,result);
-            currentPath.pop_back();
+        for(int neigh:graph[curr]){
+            currPath.push_back(neigh);
+            dfs(neigh,target,graph,currPath,result);
+            currPath.pop_back();
         }
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n=graph.size();
         vector<vector<int>> result;
-        vector<int> currentPath;
-
-        int target=graph.size()-1;
-        currentPath.push_back(0);
-        dfs(graph,0,target,currentPath,result);
+        vector<int> currPath;
+        currPath.push_back(0);
+        dfs(0,n-1,graph,currPath,result);
         return result;
     }
 };
