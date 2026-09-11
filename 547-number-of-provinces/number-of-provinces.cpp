425a871ad25c098fa92e023c882db14fe2e6bed9
Varun Gupta <varun.vip2811@gmail.com>
2026-09-11 13:17:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (92.21%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
-    vector<int> parent;
-    vector<int> size;
-    int find(int u){
-        if(u==parent[u]){
-            return u;
-        }
-        return parent[u]=find(parent[u]);
-    }
-    void Union(int u,int v){
-        int pu=find(u);
-        int pv=find(v);
-
-        if(pu==pv) return;
-
-        if(size[pu] > size[pv]){
-            parent[pv] = pu;
-            size[pu] += size[pv];
-        }
-        else{
-            parent[pu] = pv;
-            size[pv] += size[pu];
-        }
-    }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        for(int i=0;i<isConnected.size();i++){
-            parent.push_back(i);
-            size.push_back(1);
-        }
-        for(int u=0;u<isConnected.size();u++){
-            for(int v=0;v<isConnected.size();v++){
-                if(isConnected[u][v] == 1 && u != v){
-                    Union(u,v);
-                }
+    int n;
+    void dfs(int node,vector<vector<int>> &adj,vector<bool> &visited){
+        visited[node]=true;
+        for(int j=0;j<n;j++){
+            if(adj[node][j]==1 && !visited[j]){
+                dfs(j,adj,visited);
             }
         }
-        int count=0;
-        for(int i=0;i<parent.size();i++){
-            if(i==parent[i]) count++;
+    }
+    int findCircleNum(vector<vector<int>>& adj) {
+       n=adj.size();
+       vector<bool> visited(n,false);
+       int count=0;
+       for(int i=0;i<n;i++){
+        if(!visited[i]){
+            dfs(i,adj,visited);
+            count++;
         }
-        return count;
+       }
+       return count;
     }
 };
