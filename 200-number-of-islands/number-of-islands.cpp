d633ccb0ce096b32cc0ee6471c857c859d692817
Varun Gupta <varun.vip2811@gmail.com>
2026-09-13 15:49:17 +0530

    Time: 29 ms (40.57%) | Memory: 23.6 MB (24.08%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,80 +1,42 @@
 class Solution {
 public:
-
-    vector<int> parent;
-    vector<int> size;
-    int find(int node) {
-        if(node == parent[node])
-            return node;
-
-        return parent[node] = find(parent[node]);
+    int n,m;
+    bool isValid(int i,int j){
+        return i>=0 && i<n && j>=0 && j<m;
     }
-    void Union(int u, int v) {
-
-        int pu = find(u);
-        int pv = find(v);
-        if(pu == pv)
-            return;
-
-        if(size[pu] > size[pv]) {
-            parent[pv] = pu;
-            size[pu] += size[pv];
+    void bfs(int r,int c,vector<vector<char>> &grid,vector<vector<bool>>&visited){
+        visited[r][c]=true;
+        queue<pair<int,int>>q;
+        q.push({r,c});
+        int dr[]={-1,0,1,0};
+        int dc[]={0,-1,0,1};
+        while(!q.empty())
+        {
+            auto [row,col]=q.front();
+            q.pop();
+            for(int ind=0;ind<4;ind++){
+                int newr=row+dr[ind];
+                int newc=col+dc[ind];
+                if(isValid(newr,newc) && grid[newr][newc]=='1' && !visited[newr][newc]){
+                    q.push({newr,newc});
+                    visited[newr][newc]=true;
+                }
+            }
         }
-        else {
-            parent[pu] = pv;
-            size[pv] += size[pu];
-        }
     }
-
-    bool isValid(int r, int c, int n, int m) {
-        return r >= 0 && r < n &&
-               c >= 0 && c < m;
-    }
-
     int numIslands(vector<vector<char>>& grid) {
-
-        int n = grid.size();
-        int m = grid[0].size();
-        parent.resize(n * m);
-        size.assign(n * m, 1);
-
-        for(int i = 0; i < n * m; i++) {
-            parent[i] = i;
-        }
-
-        int cnt = 0;
-
-        for(int i = 0; i < n; i++) {
-             for(int j = 0; j < m; j++) {
-                 if(grid[i][j] == '1') {
-                    cnt++;
+        n=grid.size();
+        m=grid[0].size();
+        int count=0;
+        vector<vector<bool>>visited(n,vector<bool>(m,false));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && !visited[i][j]){
+                    bfs(i,j,grid,visited);
+                    count++;
                 }
             }
         }
-        int dr[] = {-1, 0, 1, 0};
-        int dc[] = {0, 1, 0, -1};
-
-        for(int row = 0; row < n; row++) {
-            for(int col = 0; col < m; col++) {
-                if(grid[row][col] == '0')
-                    continue;
-                int node = row * m + col;
-                for(int ind = 0; ind < 4; ind++) {
-                    int adjr = row + dr[ind];
-                    int adjc = col + dc[ind];
-                    if(isValid(adjr, adjc, n, m)) {
-                        if(grid[adjr][adjc] == '1') {
-                            int adjNode = adjr * m + adjc;
-                            if(find(node) != find(adjNode)) {
-                                cnt--;
-                                Union(node, adjNode);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return cnt;
+        return count;
     }
 };
